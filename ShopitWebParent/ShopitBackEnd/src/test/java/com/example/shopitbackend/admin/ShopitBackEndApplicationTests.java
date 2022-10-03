1ce165ae96ad24cf7e37723e4acd288c11a67a4c
petko.lyutskanov <simona2000petko@abv.bg>
2022-10-03 10:31:08 +0300

    Multi - module project created

---
+++ ShopitWebParent/ShopitBackEnd/src/test/java/com/example/shopitbackend/admin/ShopitBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopitbackend.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopitBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ShopitWebParent/ShopitFrontEnd/src/main/java/com/example/shopitfrontend/site/ShopitFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopitfrontend.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopitFrontEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopitFrontEndApplication.class, args);
+    }
+
+}
