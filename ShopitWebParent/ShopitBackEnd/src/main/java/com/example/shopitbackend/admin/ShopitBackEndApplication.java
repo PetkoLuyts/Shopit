93cc32d2105f74075d62138e58de5f1fa0d62131
petko.lyutskanov <simona2000petko@abv.bg>
2022-10-04 19:44:48 +0300

    Role entity, role repository and unit tests implemented

--- ShopitWebParent/ShopitBackEnd/src/main/java/com/example/shopitbackend/admin/ShopitBackEndApplication.java
+++ ShopitWebParent/ShopitBackEnd/src/main/java/com/example/shopitbackend/admin/ShopitBackEndApplication.java
@@ -2,8 +2,10 @@ package com.example.shopitbackend.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.example.common.entity", "com.example.shopitbackend.admin.user"})
 public class ShopitBackEndApplication {
 
 	public static void main(String[] args) {
---
+++ ShopitWebParent/ShopitBackEnd/src/test/java/com/example/shopitbackend/admin/RoleRepositoryTests.java
@@ -0,0 +1,42 @@
+package com.example.shopitbackend.admin;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.common.entity.Role;
+import com.example.shopitbackend.admin.user.RoleRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class RoleRepositoryTests {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Test
+    public void testCreateFirstRole() {
+        Role roleAdmin = new Role("admin", "manage everything");
+        Role savedRole = roleRepository.save(roleAdmin);
+        assertThat(savedRole.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testCreateRestRoles() {
+        Role roleSalesperson = new Role("Salesperson", "manage product price, " +
+                "customers, shipping, orders and sales report");
+        Role roleEditor = new Role("Editor", "manage categories, brands, " +
+                "products, articles and menus");
+        Role roleShipper= new Role("Shipper", "view products, " +
+                "view orders and update order status");
+        Role roleAssistant = new Role("Assistant", "manage questions and reviews");
+
+        roleRepository.saveAll(List.of(roleSalesperson, roleEditor, roleShipper, roleAssistant));
+    }
+}
