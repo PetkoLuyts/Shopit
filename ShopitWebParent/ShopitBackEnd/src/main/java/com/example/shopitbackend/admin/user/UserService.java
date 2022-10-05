847d747f23456137084258904bdb1f3e717bab51
petko.lyutskanov <simona2000petko@abv.bg>
2022-10-05 18:52:29 +0300

    Users listing function implemented

---
+++ ShopitWebParent/ShopitBackEnd/src/main/java/com/example/shopitbackend/admin/user/UserService.java
@@ -0,0 +1,18 @@
+package com.example.shopitbackend.admin.user;
+
+import com.example.common.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> listAll() {
+        return (List<User>) userRepository.findAll();
+    }
+}
