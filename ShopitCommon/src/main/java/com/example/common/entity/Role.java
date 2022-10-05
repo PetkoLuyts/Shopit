f1a797e75b3232fe5d0cd32cc211fb944f7784ab
petko.lyutskanov <simona2000petko@abv.bg>
2022-10-05 12:39:35 +0300

    User entity, user repository and unit tests implemented

--- ShopitCommon/src/main/java/com/example/common/entity/Role.java
+++ ShopitCommon/src/main/java/com/example/common/entity/Role.java
@@ -1,6 +1,7 @@
 package com.example.common.entity;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "roles")
@@ -16,6 +17,10 @@ public class Role {
     private String description;
 
     public Role() {
+    }
+
+    public Role(Integer id) {
+        this.id = id;
     }
 
     public Role(String name) {
@@ -49,5 +54,25 @@ public class Role {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id.equals(role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "name='" + name + '\'' +
+                '}';
     }
 }
---
+++ ShopitWebParent/ShopitBackEnd/src/test/java/com/example/shopitbackend/admin/UserRepositoryTests.java
@@ -0,0 +1,88 @@
+package com.example.shopitbackend.admin;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.common.entity.Role;
+import com.example.common.entity.User;
+import com.example.shopitbackend.admin.user.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testCreateNewUserWithOneRole() {
+        Role roleAdmin = entityManager.find(Role.class, 1);
+        User testUser = new User("user@abv.bg", "1234", "User", "Test");
+        testUser.addRole(roleAdmin);
+
+        User savedUser = userRepository.save(testUser);
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testCreateNewUserWithTwoRoles() {
+        User testUserWithTwoRoles = new User("test@abv.bg", "121212", "Test", "Test");
+        Role roleEditor = new Role(3);
+        Role roleAssistant = new Role(5);
+
+        testUserWithTwoRoles.addRole(roleEditor);
+        testUserWithTwoRoles.addRole(roleAssistant);
+
+        User savedUser = userRepository.save(testUserWithTwoRoles);
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testListAllUsers() {
+        Iterable<User> listUsers = userRepository.findAll();
+        listUsers.forEach(System.out::println);
+    }
+
+    @Test
+    public void testGetUserById() {
+        User user = userRepository.findById(1).get();
+        System.out.println(user);
+        assertThat(user).isNotNull();
+    }
+
+    @Test
+    public void testUpdateUserDetails() {
+        User user = userRepository.findById(1).get();
+        user.setEnabled(true);
+        user.setEmail("useruser123@abv.bg");
+
+        userRepository.save(user);
+    }
+
+    @Test
+    public void testUpdateUserRoles() {
+        User user = userRepository.findById(2).get();
+        Role roleEditor = new Role(3);
+        Role roleSalesPerson = new Role(2);
+
+        user.getRoles().remove(roleEditor);
+        user.addRole(roleSalesPerson);
+
+        userRepository.save(user);
+    }
+
+    @Test
+    public void testDeleteUser() {
+        Integer userId = 2;
+        userRepository.deleteById(2);
+    }
+}
