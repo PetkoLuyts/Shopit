9abbc37a539cf4b3ce5e2704f9c4d0dae4bda40d
petko.lyutskanov <simona2000petko@abv.bg>
2022-10-20 18:10:36 +0300

    Export users to CSV implemented

--- ShopitWebParent/ShopitBackEnd/src/main/java/com/example/shopitbackend/admin/user/UserController.java
+++ ShopitWebParent/ShopitBackEnd/src/main/java/com/example/shopitbackend/admin/user/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -159,5 +160,12 @@ public class UserController {
         redirectAttributes.addFlashAttribute("message", message);
 
         return "redirect:/users";
+    }
+
+    @GetMapping("/users/export/csv")
+    public void exportToCSV(HttpServletResponse response) throws IOException {
+        List<User> listUsers = userService.listAll();
+        UserCsvExporter exporter = new UserCsvExporter();
+        exporter.export(listUsers, response);
     }
 }
